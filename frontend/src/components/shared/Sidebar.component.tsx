22f008bbd7ddc1020cc84fab8d917bc8872ee154
CynicalMan <omar.eladly2020@gmail.com>
2024-07-25 00:38:48 +0300

    fix(dashboard): removed redundant home link

--- frontend/src/components/shared/Sidebar.component.tsx
+++ frontend/src/components/shared/Sidebar.component.tsx
@@ -1,4 +1,4 @@
-import {  Home, FormatListBulleted, PostAdd }from "@mui/icons-material"
+import {  FormatListBulleted, PostAdd }from "@mui/icons-material"
 import React from "react"
 import { Link } from "react-router-dom"
 import ThemePicker from "./ThemePicker"
@@ -9,23 +9,6 @@ const Sidebar : React.FC = () => {
         <>
             <div className={`h-[calc(100vh-48px)] w-full max-w-[4rem] md:max-w-[15rem] bg-primary sticky top-[48px]`}>
                 <div className="p-2 lg:p-5 md:p-0 text-secondary">
-                    <div className="mt-2">
-                        <h3 className="text-xs hidden md:block text-secondary">
-                            Dashboard
-                        </h3>
-                        <ul className="p-1 list-none">
-                            <li>
-                                <Link to="/" className=" border-solid border border-secondary p-1 cursor-pointer flex items-center justify-center md:justify-start rounded-lg">
-                                    <div className="rounded-lg p-1 ">
-                                        <Home  />
-                                    </div>
-                                    <span className="ml-1 hidden md:block ">
-                                        All Items
-                                    </span>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
                     <div className="mt-2">
                         <h3 className="text-xs hidden md:block text-secondary">
                             Items
